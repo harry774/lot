a093bca991fc4489f2fa33163ce44d78185d2baf
Hardik Patel <xylo.hardik@gmail.com>
2018-07-19 20:49:42 +0530

    Update settings.py

--- lotus/settings.py
+++ lotus/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
