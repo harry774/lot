6b281100ba3e7774efae75ab0ad174718b6ec28a
Hardik Patel <xylo.hardik@gmail.com>
2018-07-19 20:04:48 +0530

    Update settings.py

--- lotus/settings.py
+++ lotus/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
