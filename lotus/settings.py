70df67e528549edb8b6449e758bb922d4690c75c
Hardik Patel <xylo.hardik@gmail.com>
2018-07-24 15:22:12 +0530

    Update settings.py

--- lotus/settings.py
+++ lotus/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 #import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,4 @@ STATIC_URL = '/static/'
  #   os.path.join(PROJECT_ROOT, 'static'),
 #)
 #django_heroku.settings(locals())
+django_heroku.settings(locals())
