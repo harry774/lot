676e490620e318d4338c6a029c2b1110aeaa19ae
Hardik Patel <xylo.hardik@gmail.com>
2018-07-26 11:03:18 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     # /music/<album_id>
     path('', views.index),
     path('<int:current_status>/', views.store),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
